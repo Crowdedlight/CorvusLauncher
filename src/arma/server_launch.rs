24ea272bae0f4e50875914a81bb4881e97422567
Crowdedlight <crowdedhd@gmail.com>
2025-10-09 01:06:02 +0200

    functions to find server config and reading it to try and find the server password

--- src/arma/server_launch.rs
+++ src/arma/server_launch.rs
@@ -42,3 +42,44 @@ pub fn find_bikey(path: PathBuf) -> Result<Vec<PathBuf>> {
         false => Ok(keys),
     }
 }
+
+pub fn launch_hc() -> Result<()> {
+    // TODO server password for HCs? Or should we just steal that from the server config file. We know the A3root ;-) ?
+
+
+
+    Ok(())
+}
+/// Find the .cfg file in the A3Root to parse it and get the password
+pub fn find_config(a3root: PathBuf) -> Result<PathBuf> {
+
+    let keys = std::fs::read_dir(a3root)?
+        // Filter out all those directory entries which couldn't be read
+        .filter_map(|res| res.ok())
+        // Filter out all paths with extensions other than `csv`
+        .filter_map(|f| {
+            if f.path().extension().is_some_and(|ext| ext == "cfg") {
+                Some(f)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    // should only return one, as the a3root should only have a single .cfg file
+    match keys.first() {
+        Some(keys) => {Ok(keys.path())},
+        None => {Err(anyhow::Error::msg("A3 Server Config not found in A3Root"))}
+    }
+}
+
+/// Given the path to the A3 server config file, extract the password for use in HCs launch parameters
+pub fn get_server_password_from_config(a3_config: PathBuf) -> Result<String> {
+    // find the password which is given in the file as: password = "thepassword";
+    let config_string = std::fs::read_to_string(a3_config)?;
+
+    // TODO regex to find and extract the password string from: password = "thepassword";
+    config_string
+
+    Ok("".to_string())
+}
